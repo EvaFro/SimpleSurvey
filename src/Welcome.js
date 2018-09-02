5ca8f422be150b119f59bb8cd4b41f10febe71b8
EvaFro <frolli.erin@gmail.com>
2018-09-02 16:57:11 -0700

    create welcome component

---
+++ src/Welcome.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Welcome = () => (
+  <div>
+    <h1>Welcome To the Comic Survey</h1>
+    <div>To take this survey please select the button below.</div>
+    <button>Survey</button>
+  </div>
+);
+
+export default Welcome;
