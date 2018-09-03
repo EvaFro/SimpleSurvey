e1d7e04f40be151af66366f0c22ef5be24b2b64d
EvaFro <frolli.erin@gmail.com>
2018-09-02 19:20:44 -0700

    save survey qestion data

---
+++ src/data.js
@@ -0,0 +1,44 @@
+const questions = [
+  {
+    num: 1,
+    q: 'Lightning never strikes in the same place twice.'
+  },
+  {
+    num: 2,
+    q: 'If you cry in space the tears just stick to your face.'
+  },
+  {
+    num: 3,
+    q: 'If you cut an earthworm in half, both halves can regrow their body.'
+  },
+  {
+    num: 4,
+    q: 'Humans can distinguish between over a trillion different smells.'
+  },
+  {
+    num: 5,
+    q: 'Adults have fewer bones than babies do.'
+  },
+  {
+    num: 6,
+    q: 'Goldfish only have a memory of three seconds.'
+  },
+  {
+    num: 7,
+    q: 'There are more cells of bacteria in your body than there are human cells.'
+  },
+  {
+    num: 8,
+    q: 'Your fingernails and hair keep growing after you die.'
+  },
+  {
+    num: 9,
+    q: 'Birds are dinosaurs.'
+  },
+  {
+    num: 10,
+    q: 'Buzz Aldrin was the first man to urinate on the moon.'
+  }
+]
+
+module.exports = questions;
