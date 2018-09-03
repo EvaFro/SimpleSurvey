71bc99d19fdb88673846a38d361338e915e6ecc7
EvaFro <frolli.erin@gmail.com>
2018-09-02 20:33:09 -0700

    refactored data to inculde count data

--- src/data.js
+++ src/data.js
@@ -1,43 +1,63 @@
 const questions = [
   {
     num: 1,
-    q: 'Lightning never strikes in the same place twice.'
+    q: 'Lightning never strikes in the same place twice.',
+    trueCount: 0,
+    falseCount: 0
   },
   {
     num: 2,
-    q: 'If you cry in space the tears just stick to your face.'
+    q: 'If you cry in space the tears just stick to your face.',
+    trueCount: 0,
+    falseCount: 0
   },
   {
     num: 3,
-    q: 'If you cut an earthworm in half, both halves can regrow their body.'
+    q: 'If you cut an earthworm in half, both halves can regrow their body.',
+    trueCount: 0,
+    falseCount: 0
   },
   {
     num: 4,
-    q: 'Humans can distinguish between over a trillion different smells.'
+    q: 'Humans can distinguish between over a trillion different smells.',
+    trueCount: 0,
+    falseCount: 0
   },
   {
     num: 5,
-    q: 'Adults have fewer bones than babies do.'
+    q: 'Adults have fewer bones than babies do.',
+    trueCount: 0,
+    falseCount: 0
   },
   {
     num: 6,
-    q: 'Goldfish only have a memory of three seconds.'
+    q: 'Goldfish only have a memory of three seconds.',
+    trueCount: 0,
+    falseCount: 0
   },
   {
     num: 7,
-    q: 'There are more cells of bacteria in your body than there are human cells.'
+    q: 'There are more cells of bacteria in your body than there are human cells.',
+    trueCount: 0,
+    falseCount: 0
   },
   {
     num: 8,
-    q: 'Your fingernails and hair keep growing after you die.'
+    q: 'Your fingernails and hair keep growing after you die.',
+    trueCount: 0,
+    falseCount: 0
   },
   {
     num: 9,
-    q: 'Birds are dinosaurs.'
+    q: 'Birds are dinosaurs.',
+    trueCount: 0,
+    falseCount: 0
   },
   {
     num: 10,
-    q: 'Buzz Aldrin was the first man to urinate on the moon.'
+    q: 'Buzz Aldrin was the first man to urinate on the moon.',
+    trueCount: 0,
+    falseCount: 0
   }
 ]
 
