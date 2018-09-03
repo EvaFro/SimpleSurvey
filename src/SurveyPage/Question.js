5441faf3d966d56130d639625fdfc5ed04341e6b
EvaFro <frolli.erin@gmail.com>
2018-09-03 10:59:55 -0700

    relable radio buttons

--- src/SurveyPage/Question.js
+++ src/SurveyPage/Question.js
@@ -5,17 +5,17 @@ const Question = (props) => (
   <form>
       <div>
         <h4>{`${props.question.num}) ${props.question.q}`}</h4>
-        <label for="choice1">True</label>
         <input type="radio"
         id="choice1"
         name={`question${props.question.num}`}
         value={true}/>
+        <label > True</label>
         <br/>
-        <label for="choice2">False</label>
         <input type="radio"
         id="choice2"
         name={`question${props.question.num}`}
         value={false}/>
+        <label > False</label>
       </div>
   </form>
   </div>
