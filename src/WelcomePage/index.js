3efa7221150811326b475dcc3e266144289ae332
EvaFro <frolli.erin@gmail.com>
2018-09-03 10:56:32 -0700

    refactor to render each component page

--- src/WelcomePage/index.js
+++ src/WelcomePage/index.js
@@ -1,10 +1,11 @@
 import React from "react";
 import { render } from "react-dom";
 import Welcome from "./Welcome"
-// import Survey from "../SurveyPage/Survey"
-// import Results from '../ResultsPage/Results'
+import Survey from "../SurveyPage/Survey"
+import Results from '../ResultsPage/Results'
+import axios from 'axios'
 
-import questions from '../data'
+import questions from '../data/data'
 import "../styles.scss";
 
 
@@ -12,20 +13,43 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      onSave : this.onSave.bind(this),
-      answers:{}
+      currentComponent: 'Welcome',
+      questionsData:[]
     };
   }
 
-  onSave(event) {
+  componentDidMount(){
+    axios.get('/questions')
+      .then(res => this.setState({questionsData: res.data}))
+      .catch(error => console.error(error))
+  }
 
+  switchComponent(data) {
+    console.log('This is the current Component: ', data.name)
+    this.setState({
+      currentComponent: data.name,
+    });
+  }
+
+  displayPage() {
+    let page = this.state.currentComponent
+    switch(page) {
+      case 'Welcome':
+        return <Welcome onClick={data => this.switchComponent(data)}/>
+      case 'Survey':
+        return <Survey questions={this.state.questionsData} switchComponent={data => this.switchComponent(data)}/>
+      case 'Results':
+        return <Results questionsResults={this.state.questionsData} onClick={data => this.switchComponent(data)}/>
+    }
   }
 
   render() {
-    const { onSave } = this.state;
+    console.log(this.state.questionsData)
     return (
       <div>
-        <Welcome/>
+        {
+          this.displayPage()
+        }
       </div>
     );
   }
